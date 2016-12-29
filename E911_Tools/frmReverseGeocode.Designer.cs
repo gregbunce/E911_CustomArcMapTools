860474796f2f969d05909fd957c488d378ba8e13
Greg Bunce <gbunce@utah.gov>
2016-12-29 15:29:28 -0700

    reverse geocode updates

---
+++ E911_Tools/frmReverseGeocode.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace E911_Tools
+{
+    partial class frmReverseGeocode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboChooseLayer = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReverseGeocode = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cboChooseLayer
+            // 
+            this.cboChooseLayer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboChooseLayer.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.cboChooseLayer.FormattingEnabled = true;
+            this.cboChooseLayer.Location = new System.Drawing.Point(13, 54);
+            this.cboChooseLayer.Name = "cboChooseLayer";
+            this.cboChooseLayer.Size = new System.Drawing.Size(311, 21);
+            this.cboChooseLayer.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose Layer to Reverse Geocode";
+            // 
+            // btnReverseGeocode
+            // 
+            this.btnReverseGeocode.Location = new System.Drawing.Point(84, 238);
+            this.btnReverseGeocode.Name = "btnReverseGeocode";
+            this.btnReverseGeocode.Size = new System.Drawing.Size(160, 23);
+            this.btnReverseGeocode.TabIndex = 2;
+            this.btnReverseGeocode.Text = "Reverse Geocode";
+            this.btnReverseGeocode.UseVisualStyleBackColor = true;
+            this.btnReverseGeocode.Click += new System.EventHandler(this.btnReverseGeocode_Click);
+            // 
+            // frmReverseGeocode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 273);
+            this.Controls.Add(this.btnReverseGeocode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboChooseLayer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "frmReverseGeocode";
+            this.ShowIcon = false;
+            this.Text = "Reverse Geocode Mile Markers";
+            this.Load += new System.EventHandler(this.frmReverseGeocode_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboChooseLayer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReverseGeocode;
+    }
+}
---
+++ E911_Tools/frmReverseGeocode.cs
@@ -0,0 +1,163 @@
+﻿using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Location;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace E911_Tools
+{
+    public partial class frmReverseGeocode : Form
+    {
+        //IWorkspace workspaceSGID;
+        //IFeatureWorkspace featureWorkspaceSGID;
+
+        public frmReverseGeocode()
+        {
+            InitializeComponent();
+        }
+
+        // form load method
+        private void frmReverseGeocode_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // get access to the current arcmap variables
+                clsE911StaticClass.GetCurrentMapDocVariables();
+
+                // load the choose layer combobox with the map's layer names
+                for (int i = 0; i < clsE911Globals.pMap.LayerCount; i++)
+                {
+                    cboChooseLayer.Items.Add(clsE911Globals.pMap.Layer[i].Name.ToString());
+                }
+
+                // get access to sgid database
+                //workspaceSGID = clsE911StaticClass.ConnectToTransactionalVersion("", "sde:sqlserver:sgid.agrc.utah.gov", "SGID10", "DBMS", "sde.DEFAULT", "agrc", "agrc");
+                //featureWorkspaceSGID = (IFeatureWorkspace)workspaceSGID;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Message: " + Environment.NewLine + ex.Message + Environment.NewLine + Environment.NewLine +
+                "Error Source: " + Environment.NewLine + ex.Source + Environment.NewLine + Environment.NewLine +
+                "Error Location:" + Environment.NewLine + ex.StackTrace,
+                "E911 Tool error!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+
+
+        // reverse geocode button
+        private void btnReverseGeocode_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // show the cursor as busy
+                System.Windows.Forms.Cursor.Current = Cursors.WaitCursor;
+
+
+                // get access to the layer that is specified in the choose layer dropdown box
+                IGeoFeatureLayer pGFlayer = null;
+                IFeatureLayer arcFeatLayer = null;
+                // loop through the map's layers and check for the layer with the targeted name (based on the choose layer combobox)
+                for (int i = 0; i < clsE911Globals.pMap.LayerCount; i++)
+                {
+                    if (clsE911Globals.pMap.Layer[i].Name == cboChooseLayer.Text)
+                    {
+                        pGFlayer = (IGeoFeatureLayer)clsE911Globals.pMap.Layer[i];
+                        arcFeatLayer = (IFeatureLayer)clsE911Globals.pMap.Layer[i];
+                    }
+                }
+
+                // check to assure the user chose a point layer
+                if (arcFeatLayer.FeatureClass.ShapeType != ESRI.ArcGIS.Geometry.esriGeometryType.esriGeometryPoint)
+                {
+                    MessageBox.Show("You must choose a Point Layer to Reverse Geocode.", "Must Be Point Layer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+  
+                // Get a locator from the locator Workspace
+                // Set up the SDE connection properties 
+                IPropertySet connectionProperties = new PropertySetClass();
+                connectionProperties.SetProperty("SERVER", "");
+                //propertySet.SetProperty("DBCLIENT", dbclient);
+                connectionProperties.SetProperty("INSTANCE", "sde:sqlserver:sgid.agrc.utah.gov");
+                connectionProperties.SetProperty("DATABASE", "SGID10");
+                connectionProperties.SetProperty("AUTHENTICATION_MODE", "DBMS");
+                connectionProperties.SetProperty("VERSION", "sde.DEFAULT");
+                connectionProperties.SetProperty("USER", "agrc");
+                connectionProperties.SetProperty("PASSWORD", "agrc");
+
+                // Get the Workspace
+                System.Object obj = Activator.CreateInstance(Type.GetTypeFromProgID("esriDataSourcesGDB.SdeWorkspaceFactory"));
+                IWorkspaceFactory2 workspaceFactory = obj as IWorkspaceFactory2;
+                IWorkspace workspace = workspaceFactory.Open(connectionProperties, 0);
+
+                obj = Activator.CreateInstance(Type.GetTypeFromProgID("esriLocation.LocatorManager"));
+                ILocatorManager2 locatorManager = obj as ILocatorManager2;
+                ILocatorWorkspace locatorWorkspace = locatorManager.GetLocatorWorkspace(workspace);
+                //IDatabaseLocatorWorkspace databaseLocatorWorkspace = (IDatabaseLocatorWorkspace)locatorWorkspace;
+
+                IReverseGeocoding reverseGeocoding = (IReverseGeocoding)locatorWorkspace.GetLocator("TRANSPORTATION.Locator_AddressPtsAddrSys");
+
+                // Set the search tolerance for reverse geocoding
+                IReverseGeocodingProperties reverseGeocodingProperties = (IReverseGeocodingProperties)reverseGeocoding;
+                reverseGeocodingProperties.SearchDistance = 100;
+                reverseGeocodingProperties.SearchDistanceUnits = esriUnits.esriFeet;
+
+                
+
+                // now loop through the point feature layer
+                IFeatureCursor arcFeatCur = arcFeatLayer.Search(null, false);
+                IFeature arcFeature;
+                
+                while ((arcFeature = arcFeatCur.NextFeature()) != null)
+                {
+
+                    IPoint arcPoint = (IPoint)arcFeature.ShapeCopy;
+
+                    // Find the address nearest the Point
+                    IPropertySet addressProperties = reverseGeocoding.ReverseGeocode(arcPoint, false);
+
+                    // Print the address properties
+                    IAddressInputs addressInputs = (IAddressInputs)reverseGeocoding;
+                    IFields addressFields = addressInputs.AddressFields;
+                    for (int i = 0; i < addressFields.FieldCount; i++)
+                    {
+                        IField addressField = addressFields.get_Field(i);
+                        MessageBox.Show(addressField.AliasName + ": " + addressProperties.GetProperty(addressField.Name));
+                    }
+
+
+
+
+                }
+
+
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Message: " + Environment.NewLine + ex.Message + Environment.NewLine + Environment.NewLine +
+                "Error Source: " + Environment.NewLine + ex.Source + Environment.NewLine + Environment.NewLine +
+                "Error Location:" + Environment.NewLine + ex.StackTrace,
+                "E911 Tool error!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+
+    }
+}
